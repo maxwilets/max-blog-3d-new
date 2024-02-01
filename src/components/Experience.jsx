c98938f89b51f36f05544447b33391a593e4ba6f
maxwilets <mwilets@gmail.com>
2024-02-01 15:53:27 -0700

    work models added

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -14,10 +14,13 @@ import { textVariant } from '../utils/motion';
 
 const ExperienceCard = ({ experience }) => (
   <VerticalTimelineElement
-    contentStyle={{ background: '#1d1836', color: '#f' }}
-    contentArrowStyle={{ borderRIght: '7px solid' }}
+    contentStyle={{
+      background: '#1d1836',
+      color: '#fff',
+    }}
+    contentArrowStyle={{ borderRight: '7px solid  #232631' }}
     date={experience.date}
-    iconStyle={{ background: experience.bg }}
+    iconStyle={{ background: experience.iconBg }}
     icon={
       <div className="flex justify-center items-center w-full h-full">
         <img
@@ -30,7 +33,23 @@ const ExperienceCard = ({ experience }) => (
   >
     <div>
       <h3 className="text-white text-[24px] font-bold">{experience.title}</h3>
+      <p
+        className="text-secondary text-[16px] font-semibold"
+        style={{ margin: 0 }}
+      >
+        {experience.company_name}
+      </p>
     </div>
+    <ul className="mt-5 list-disc ml-5 space-y-2">
+      {experience.points.map((point, index) => (
+        <li
+          key={`experience-point-${index}`}
+          className="text-white-100 text-[14px] pl-1 tracking-wider"
+        >
+          {point}
+        </li>
+      ))}
+    </ul>
   </VerticalTimelineElement>
 );
 
