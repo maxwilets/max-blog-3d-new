88519177adac23256d0eea1b55aeda3a96a98d7c
maxwilets <mwilets@gmail.com>
2024-02-01 15:01:22 -0700

    reinit repository

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-# max-blog-3d-new
+# Max Porfolio 3d
+This is a test of three.js and threejsfiber to work with using 3d web developing built alongside [this video](https://www.youtube.com/watch?v=0fYi8SGA20k)
---
+++ src/components/Experience.jsx
@@ -0,0 +1,53 @@
+import React from 'react';
+import {
+  VerticalTimeline,
+  VerticalTimelineElement,
+} from 'react-vertical-timeline-component';
+import { motion } from 'framer-motion';
+
+import 'react-vertical-timeline-component/style.min.css';
+
+import { styles } from '../style';
+import { experiences } from '../constants';
+import { SectionWrapper } from '../hoc';
+import { textVariant } from '../utils/motion';
+
+const ExperienceCard = ({ experience }) => (
+  <VerticalTimelineElement
+    contentStyle={{ background: '#1d1836', color: '#f' }}
+    contentArrowStyle={{ borderRIght: '7px solid' }}
+    date={experience.date}
+    iconStyle={{ background: experience.bg }}
+    icon={
+      <div className="flex justify-center items-center w-full h-full">
+        <img
+          src={experience.icon}
+          alt={experience.company_name}
+          className="w-[60%] h-[60%] object-contain"
+        />
+      </div>
+    }
+  >
+    <div>
+      <h3 className="text-white text-[24px] font-bold">{experience.title}</h3>
+    </div>
+  </VerticalTimelineElement>
+);
+
+const Experience = () => (
+  <>
+    <motion.div variants={textVariant()}>
+      <p className={styles.sectionSubText}>What I have done so far</p>
+      <h2 className={styles.sectionHeadText}>Work Experience.</h2>
+    </motion.div>
+    <div className="mt-20 flex flex-col">
+      <VerticalTimeline>
+        {experiences.map((experience, index) => (
+          <ExperienceCard key={index} experience={experience} />
+        ))}
+      </VerticalTimeline>
+    </div>
+  </>
+);
+
+export default SectionWrapper(Experience, 'work');
